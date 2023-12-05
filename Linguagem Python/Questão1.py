c7140abb67fe33070fbeeea0fd5fb0398971a030
Sofazul <84977962+Sofazul@users.noreply.github.com>
2023-12-05 11:07:32 -0300

    Update Questão1.py

--- Linguagem Python/Questão1.py
+++ Linguagem Python/Questão1.py
@@ -3,6 +3,6 @@ velmot = int(input("digite a velocidade do motorista:"))
 multa = 0
 if velmot > velmax:
     multa = 5*(velmot-velmax)
+    print(f'A multa será de {multa:.2f}')
 else:
     print("Não há multa")
-    print(f'A multa será de {multa:.2f}')
